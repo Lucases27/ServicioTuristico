3af2f4698de533c8d3aa6d4e1762cefb793de2d3
Lucas Emiliano Saleme <75321699+Lucases27@users.noreply.github.com>
2021-12-17 06:30:37 -0300

    Controllers
    Crear las controladores del paquete lógica y persistencia.

---
+++ src/java/logica/Controladora.java
@@ -0,0 +1,8 @@
+package logica;
+
+import persistencia.ClienteJpaController;
+import persistencia.ControladoraPersistencia;
+
+public class Controladora {
+    private ControladoraPersistencia controlPersis = new ControladoraPersistencia();
+}
---
+++ src/java/persistencia/ControladoraPersistencia.java
@@ -0,0 +1,17 @@
+package persistencia;
+
+public class ControladoraPersistencia {
+    private ClienteJpaController clienteJPA = new ClienteJpaController();
+    private EmpleadoJpaController empleadoJPA = new EmpleadoJpaController();
+    private PaqueteJpaController paqueteJPA = new PaqueteJpaController();
+    private ServicioJpaController servicioJPA = new ServicioJpaController();
+    private UsuarioJpaController usuarioJPA = new UsuarioJpaController();
+    private VentaJpaController ventaJPA = new VentaJpaController();
+    
+    
+    
+    
+    
+    
+    
+}
